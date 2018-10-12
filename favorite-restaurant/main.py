d151728318a3c66464a779699d8c0e2e35190bc1
tetsuromori <tetsuromori@gmail.com>
2018-10-12 16:10:27 +0900

    Update main.py

--- favorite-restaurant/main.py
+++ favorite-restaurant/main.py
@@ -1,3 +1,8 @@
 import controllers.conversation
 
-controller.conversation.talk_about_restaurant()
+
+def main():
+    controller.conversation.talk_about_restaurant()
+
+if __name__ == '__main__':
+    main()
