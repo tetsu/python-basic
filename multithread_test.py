73fe750a9a3e8d213dfeb552b4b9dcb919814679
tetsuromori <tetsuromori@gmail.com>
2018-10-29 16:18:45 +0900

    Add barrier scenario to threading

--- multithread_test.py
+++ multithread_test.py
@@ -104,6 +104,42 @@ def worker11_3(event):
     logging.debug('end')
     event.set() # event starts here
 
+def worker12_1(condition):
+    with condition:
+        condition.wait() # wait until condition gets notified
+        logging.debug('start')
+        time.sleep(2)
+        logging.debug('end')
+
+def worker12_2(condition):
+    with condition:
+        condition.wait() # wait until prefios condition ends
+        logging.debug('start')
+        time.sleep(2)
+        logging.debug('end')
+
+def worker12_3(condition):
+    with condition:
+        logging.debug('start')
+        logging.debug('end')
+        condition.notifyAll() # conditions get notified here
+
+def worker13_1(barrier):
+    r = barrier.wait()
+    logging.debug('num={}'.format(r))
+    while True:
+        logging.debug('start')
+        time.sleep(2)
+        logging.debug('end')
+
+def worker13_2(barrier):
+    r = barrier.wait()
+    logging.debug('num={}'.format(r))
+    while True:
+        logging.debug('start')
+        time.sleep(2)
+        logging.debug('end')
+
 def counter():
     i = 1
     while True:
@@ -129,6 +165,7 @@ if __name__ == '__main__':
     t1.join()
     t2.join()
 
+
     print('\n**** 2. thread with for-loop and daemon ******\n')
     threads = []
     for _ in range(5):
@@ -141,6 +178,7 @@ if __name__ == '__main__':
         threads.append(t)
     for thread in threads:
         thread.join()
+
 
     print('\n***** 3. Using Daemon and Threading Enumerate *****\n')
     for index, _ in enumerate(range(5)):
@@ -162,6 +200,7 @@ if __name__ == '__main__':
     t.start()
     t.join()
 
+
     print('\n***** 5. Lock thread so that different thread won\'t interrupt *****\n')
     d = {'x': 0}
     lock = threading.Lock()
@@ -179,6 +218,7 @@ if __name__ == '__main__':
     t2.start()
     t1.join()
     t2.join()
+
 
     print('\n***** 6. RLock thread to acquire a lock within a lock *****\n')
     d = {'x': 0}
@@ -198,6 +238,7 @@ if __name__ == '__main__':
     t1.join()
     t2.join()
 
+
     print('\n***** 7. with semaphore to start 2 locked threads first *****\n')
     semaphore = threading.Semaphore(2)
     t1 = threading.Thread(name='thread7_1', target=worker6, args=(semaphore,))
@@ -210,6 +251,7 @@ if __name__ == '__main__':
     t1.join()
     t2.join()
     t3.join()
+
 
     print('\n***** 8. control thread with queue *****\n')
     q = queue.Queue()
@@ -234,6 +276,7 @@ if __name__ == '__main__':
     q.put(None)
     t1.join()
 
+
     print('\n***** 10. queue with simultaniously running threads *****\n')
     q = queue.Queue()
     for index in range(100):
@@ -249,6 +292,7 @@ if __name__ == '__main__':
     for _ in range(len(ts)):
         q.put(None)
     [t.join() for t in ts]
+
 
     print('\n***** 11. Using Event *****\n')
     event = threading.Event()
@@ -273,3 +317,46 @@ if __name__ == '__main__':
     t1.join()
     t2.join()
     t3.join()
+
+
+    print('\n***** 12. Using Condition *****\n')
+    condition = threading.Condition()
+    t1 = threading.Thread(
+        name='thread12_1',
+        target=worker12_1,
+        args=(condition,)
+    )
+    t2 = threading.Thread(
+        name='thread12_2',
+        target=worker12_2,
+        args=(condition,)
+    )
+    t3 = threading.Thread(
+        name='thread12_3',
+        target=worker12_3,
+        args=(condition,)
+    )
+    t1.start()
+    t2.start()
+    t3.start()
+    t1.join()
+    t2.join()
+    t3.join()
+
+
+    print('\n***** 13. Using Barrier *****\n')
+    barrier = threading.Barrier(2)
+    t1 = threading.Thread(
+        name='thread13_1',
+        target=worker13_1,
+        args=(barrier,)
+    )
+    t2 = threading.Thread(
+        name='thread13_2',
+        target=worker13_2,
+        args=(barrier,)
+    )
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
